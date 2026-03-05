af417dcf4ec6c15976b2d7284ed56c5e82bdf45f
Iona Xia <ionaxia@stanford.edu>
2026-03-05 00:06:51 -0800

    make login more complex

--- back_end/civitas/settings.py
+++ back_end/civitas/settings.py
@@ -155,6 +155,15 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+    {
+        'NAME': 'contracts.validators.UppercaseValidator',
+    },
+    {
+        'NAME': 'contracts.validators.LowercaseValidator',
+    },
+    {
+        'NAME': 'contracts.validators.SpecialCharacterValidator',
+    },
 ]
 
 
